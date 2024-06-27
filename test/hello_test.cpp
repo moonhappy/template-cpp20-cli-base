e0dd9a4da4f6412544ac5997d149c04ca5f76c3f
Phillip C <phizzajc@gmail.com>
2024-06-27 16:59:22 +0800

    use more appropriate test

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,17 +1,15 @@
 //
 // C++20 CLI Template
-// Copyright (c) Phillip Cooper | https://moonhappy.io
+// Copyright (c) Moon Happy / Phillip Cooper.
 //
 // This code is licensed under MIT license (see LICENSE for details)
 //
 
 #include <gtest/gtest.h>
-
+#include "hello/hello.hpp"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  // Expect two strings to be equal.
+  EXPECT_STREQ(hello_world().c_str(), "Hello, World! r = 400");
 }
